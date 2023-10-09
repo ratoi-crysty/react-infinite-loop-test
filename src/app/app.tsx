32e7045937ad499e9f23f297c160f430d8336129
Cristian Ratoi <ratoi_crysty@yahoo.com>
2023-10-09 11:58:13 +0300

    feat: add the latest changes

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,50 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
+import { useCallback, useEffect, useState } from 'react';
+import { flushSync } from 'react-dom';
 
-import NxWelcome from './nx-welcome';
+let cachedData: Promise<number>;
+
+function getData(): Promise<number> {
+  if (!cachedData) {
+    cachedData = new Promise((resolve) => setTimeout(() => resolve(1), 1000));
+  }
+
+  return cachedData;
+}
+
+const cond = false;
+
+async function getValues(): Promise<number | undefined> {
+  if (cond) {
+    return undefined;
+  }
+
+  return 3;
+}
 
 export function App() {
+  const [obj, setObj] = useState<{ value?: number }>({});
+  const [flag, setFlag] = useState<boolean>(false);
+
+  const trigger = useCallback(() => {
+    setFlag(true);
+  }, [])
+  useEffect(() => {
+    console.log('Flag', flag);
+
+    if (!flag) {
+      return;
+    }
+    console.log('obj', obj);
+    setFlag(false);
+
+    Promise.resolve().then(() => {
+      setObj({ value: 3 });
+    });
+  }, [flag, obj]);
+
   return (
     <div>
-      <NxWelcome title="react-infinite-loop-test" />
+      <button onClick={trigger}>Trigger</button>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -9,5 +9,5 @@ const root = ReactDOM.createRoot(
 root.render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode> as any
 );
